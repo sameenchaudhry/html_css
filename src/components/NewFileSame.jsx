4c659cbc463611254af248b01bc3b70e568d34ca
faiqa <faiqaafzal7021@gmail.com>
2024-07-26 15:16:15 +0500

    added some changes

---
+++ src/components/NewFileSame.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+const NewFileSame = () => {
+  return (
+    <>
+    <h1>HTML Forms</h1>
+ <h4>If you click the "Submit" button, the form-data will be sent to a page called "/action_page.php".</h4>
+   <div>If you click the "Submit" button, the form-data will be sent to a page called "/action_page.php".</div>
+    <div>If you click the "Submit" button, the form-data will be sent to a page called "/action_page.php".</div>
+     <p>If you click the "Submit" button, the form-data will be sent to a page called "/action_page.php".</p>
+    <h2>HTML Forms</h2>
+     <button for="firstname">First name: </button>
+    <input type="text" name="firstname"  required></input>
+    <button for="firstname">LastName</button>
+    <h6>hello world</h6>
+    </>
+  )
+}
+export default NewFileSame
+
+
+
+
+
+
+
+
+
