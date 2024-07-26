95743d01a7d5d31781f1ab128bd0c3854929a7ae
sameenchaudhry <sameen.chaudhary.9@gmail.com>
2024-07-26 14:34:29 +0500

    f1

---
+++ src/components/Feature1.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Feature1 = () => {
+  return (
+    <>
+     <div>Feature1</div>
+     <h1>heading</h1>
+     <h3>heading33</h3>
+    </>
+   //////
+   ///
+   //
+   /*hahhahahaha */
+     
+  )
+}
+
+export default Feature1
